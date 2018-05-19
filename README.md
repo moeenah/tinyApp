9772e57db9ba1e80f27a3a98f56db1808cf62339
moeenah <moeen.ahmad97@hotmail.com>
2018-05-19 03:53:26 +0000

    added readme and pics

---
+++ README.md
@@ -0,0 +1,27 @@
+# TinyApp Project
+
+TinyApp is a dope full stack web application built with node
+
+## Final Product
+
+!["Screenshot of URLs page"](#)
+!["Screenshot of Login page"](#)
+!["Screenshot of Register page"](#)
+!["Screenshot of Edit page"](#)
+!["Screenshot of Add URL page"](#)
+
+# Dependencies
+
+- bcryptjs
+- body-parser
+- cookie-session
+- EJS
+- Express
+- Node.js
+
+## Getting Started
+- Install all dependencies (using 'npm install' command)
+- Run the development web server with 'node express_server.js' command
+- Enter the address given in the command line after running server
+
+
--- express_server.js
+++ express_server.js
@@ -1,6 +1,5 @@
 let express = require("express");
 
-//let cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 
 const bodyParser = require("body-parser");
@@ -14,8 +13,6 @@ let PORT = process.env.PORT || 8080;
 const salt = bcrypt.genSaltSync(10);
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-//app.use(cookieParser());
 
 app.use(cookieSession({
   name: 'session',
@@ -51,6 +48,7 @@ const users = {
   }
 };
 
+//adds url to user's object
 function urlsForUser(id) {
   let userArr = [];
   let UrlID = Object.keys(urlDatabase);
@@ -60,11 +58,10 @@ function urlsForUser(id) {
     userArr.push(urlDatabase[element]);
     }
   });
-
   return userArr;
-
 }
 
+//checks if password is part of any user's object
 function passWord(email) {
   let userIdArr = Object.keys(users);
   let result = '';
@@ -73,224 +70,8 @@ function passWord(email) {
       result = users[userID].password;
     }
   });
-
   return result;
 }
-
-//home page containing all info
-app.get('/urls', (req, res) => {
-
-  //let stuff = urlsForUser(req.cookies.user_id, urlDatabase);
-  // console.log(stuff);
-    let templateVars = { urls: urlsForUser(req.session.user_id),
-                      user_id: req.session.user_id,
-                      users: users };
-  // console.log(req.cookies.user_id);
-  // console.log(users);
-  //console.log(urlDatabase);
-  res.render('urls_index', templateVars);
-});
-
-//displays entry field for new URLs
-app.get("/urls/new", (req, res) => {
-  let templateVars = {user_id: req.session.user_id,
-                      users: users };
-  res.render("urls_new", templateVars);
-});
-
-//sends originalURL object to shortened URL page and shows the page
-app.get('/urls/:id', (req, res) => {
-  let originalURL = { long: urlDatabase[req.params.id]['url'],
-                      short: req.params.id,
-                      user_id: req.session.user_id,
-                      users: users,
-                      userID: urlDatabase[req.params.id]['userID'], };
-
-console.log(originalURL);
-  res.render('urls_show', originalURL);
-})
-
-//home page
-app.get("/", (req, res) => {
-  res.end("Hello!");
-});
-
-//sample page
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-//sample page displaying Hello World
-app.get('/hello', (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-//redirects to longURL for shortURL page
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL]['url'];
-  res.redirect(longURL);
-});
-
-//renders register page
-app.get("/register", (req, res) => {
-
-  res.render('urls_register');
-});
-
-//renders login page
-app.get("/login", (req, res) => {
-  res.render('urls_login');
-});
-
-//checks if email and pass match ones in users object and checks if fields are empty
-app.post("/login", (req, res) => {
-  //console.log(req.body.email);
-
-  if (req.body.email === '' || req.body.password === '') {
-    //console.log(req.body);
-    res.sendStatus(403);
-  }
-
-
-
-
-  let arrID = Object.keys(users);
-
-  let arrEmail = [];
-  arrID.forEach(function(element) {
-    arrEmail.push((users[element].email));
-  });
-  //console.log(arrEmail);
-
-  let arrPassword = [];
-  arrID.forEach(function(element) {
-    arrPassword.push((users[element].password));
-  });
-  //console.log(arrPassword);
-
-  if (arrEmail.includes(req.body.email) === false) {
-    //add alert to error
-        res.sendStatus(403);
-  }
-
-  else if (bcrypt.compareSync(req.body.password, passWord(req.body.email)) === false) {
-
-        res.sendStatus(403);
-  }
-  else {
-
-  let userKeys = Object.keys(users);
-  //console.log(userKeys);
-  let id = '';
-  userKeys.forEach(function(element) {
-    if (users[element]['email'] === req.body.email) {
-      id = element;
-    }
-  });
-  //console.log(id);
-  req.session.user_id = id;
-    res.redirect('/urls');
-  }
-
-});
-
-//checks if email already exists and checks if field is empty
-app.post("/register", (req, res) => {
-
-  if (req.body.email === '' || req.body.password === '') {
-    //console.log(req.body);
-    //add alert to error
-    res.sendStatus(400);
-  }
-
-  else {
-
-    let arrEmail = [];
-    let arrID = Object.keys(users);
-    arrID.forEach(function(element) {
-      arrEmail.push((users[element].email));
-    });
-    //console.log(arrEmail);
-
-      if (arrEmail.includes(req.body.email)) {
-        //add alert to error
-            res.sendStatus(400);
-      }
-
-      else {
-       let random = generateRandomString();
-        users[random] = {};
-        users[random]['id'] = random;
-        users[random]['email'] = req.body.email;
-        users[random]['password'] = bcrypt.hashSync(req.body.password, salt);
-
-        req.session.user_id = random;
-        //console.log(users);
-        res.redirect('/urls');
-      }
-  }
-});
-
-//POSTS to /urls
-app.post("/urls", (req, res) => {
-    // debug statement to see POST parameters
-  //urlDatabase['h'] = req.body;
-  let random = generateRandomString();
-  urlDatabase[random] = {}
-  urlDatabase[random]['id'] = random;
-  urlDatabase[random]['url'] = req.body.longURL;
-  urlDatabase[random]['userID'] = req.session.user_id;
-  console.log(urlDatabase);
-  //console.log(urlDatabase);
-  res.redirect(`/urls`);
-});
-
-app.post("/urls/:id/delete", (req, res) => {
-    // debug statement to see POST parameters
-  console.log(req.params.id);
-  let user_id = req.session.user_id;
-  if (user_id !== urlDatabase[req.params.id]['userID']) {
-    res.sendStatus(403);
-  }
-  else {
-    delete urlDatabase[req.params.id];
-    res.redirect('/urls');
-  }
-});
-
-app.post("/urls/:id/edit", (req, res) => {
-    // debug statement to see POST parameters
-  //console.log(req.body.longURL);
-  let user_id = req.session.user_id;
-  console.log(user_id);
-  console.log(req.params.id);
-  if (user_id !== urlDatabase[req.params.id]['userID']) {
-    res.sendStatus(403);
-  }
-  else {
-    urlDatabase[req.params.id]['url'] = req.body.longURL;
-    res.redirect('/urls');
-  }
-
-});
-
-app.post("/logout", (req, res) => {
-    // debug statement to see POST parameters
-  //console.log(req.body.longURL);
-
-  //res.clearCookie('user_id');
-  req.session.user_id = undefined;
-  res.redirect(`/urls`);
-
-});
-
-//listens to specified port
-app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}!`);
-});
-
-
-
 
 //generates random alphanumeric string
 function generateRandomString() {
@@ -299,13 +80,203 @@ function generateRandomString() {
 
   for (let i = 0; i < 6; i++) {
     let chosen = random.charAt(Math.floor(Math.random() * random.length));
-    //let replace = new RegExp(chosen);
-    //random = random.replace(replace, '');
     char += chosen;
   }
-
-  //return console.log(char + "\n" + random);
   return char;
-
 }
-//generateRandomString();
+
+app.get('/', (req, res) => {
+  if (req.session.user_id === undefined) {
+    res.redirect('/login');
+  } else {
+    res.redirect('/urls');
+  }
+});
+
+//home page containing all info
+app.get('/urls', (req, res) => {
+    let templateVars = { urls: urlsForUser(req.session.user_id),
+                      user_id: req.session.user_id,
+                      users: users };
+
+  res.render('urls_index', templateVars);
+});
+
+//displays entry field for new URLs
+app.get("/urls/new", (req, res) => {
+  if (req.session.user_id === undefined) {
+    res.redirect('/login');
+  } else {
+  let templateVars = {user_id: req.session.user_id,
+                      users: users };
+  res.render("urls_new", templateVars);
+}
+});
+
+//sends originalURL object to shortened URL page and shows the page
+app.get('/urls/:id', (req, res) => {
+  if (urlDatabase[req.params.id] === undefined) {
+    let templateVars = {error: 'does not exist',
+                        short: req.params.id,
+                        user_id: req.session.user_id,
+                        users: users,
+                        userID: urlDatabase[req.params.id] };
+    res.render('urls_show', templateVars);
+  } else {
+    let templateVars = { long: urlDatabase[req.params.id]['url'],
+                        short: req.params.id,
+                        user_id: req.session.user_id,
+                        users: users,
+                        userID: urlDatabase[req.params.id]['userID'],
+                        error: urlDatabase[req.params.id]};
+    res.status(403);
+    res.render('urls_show', templateVars);
+  }
+});
+
+//redirects to longURL for shortURL page
+app.get("/u/:shortURL", (req, res) => {
+   if (urlDatabase[req.params.shortURL] === undefined) {
+    res.status(400).send('<html><head><title>TinyAPP</title></head><body style="font-size:20px; padding:50px;"><p>Error 400</p><p>This TinyURL does not exist.</p><form action="/urls" method="GET"><input id="Home" type="submit" value="Home"/></form></body></html>');
+  } else {
+    let longURL = urlDatabase[req.params.shortURL]['url'];
+    res.redirect(longURL);
+  }
+});
+
+//renders register page
+app.get("/register", (req, res) => {
+  if (req.session.user_id === undefined) {
+    res.render('urls_register');
+  } else {
+    res.redirect('/urls');
+  }
+});
+
+//renders login page
+app.get("/login", (req, res) => {
+  if (req.session.user_id === undefined) {
+    res.render('urls_login');
+  } else {
+    res.redirect('/urls');
+  }
+});
+
+//checks if email and pass match ones in users object and checks if fields are empty
+app.post("/login", (req, res) => {
+  if (req.body.email === '' || req.body.password === '') {
+    res.status(403).send('<html><head><title>TinyAPP</title></head><body style="font-size:20px; padding:50px;"><p>Error 403</p><p>Please enter both an email and a password.</p><form action="/login" method="GET"><input id="Try again" type="submit" value="Try again"/></form></body></html>');
+  }
+  let arrID = Object.keys(users);
+
+  let arrEmail = [];
+  arrID.forEach(function(element) {
+    arrEmail.push((users[element].email));
+  });
+
+  let arrPassword = [];
+  arrID.forEach(function(element) {
+    arrPassword.push((users[element].password));
+  });
+
+  if (arrEmail.includes(req.body.email) === false) {
+    //returns error message
+    res.status(403).send('<html><head><title>TinyAPP</title></head><body style="font-size:20px; padding:50px;"><p>Error 403</p><p>Incorrect email adress.</p><form action="/login" method="GET"><input id="Try again" type="submit" value="Try again"/></form></body></html>');
+  } else if (bcrypt.compareSync(req.body.password, passWord(req.body.email)) === false) {
+    //returns error message
+    res.status(403).send('<html><head><title>TinyAPP</title></head><body style="font-size:20px; padding:50px;"><p>Error 403</p><p>Incorrest password</p><form action="/login" method="GET"><input id="Try again" type="submit" value="Try again"/></form></body></html>');
+  } else {
+    let userKeys = Object.keys(users);
+    let id = '';
+    userKeys.forEach(function(element) {
+      if (users[element]['email'] === req.body.email) {
+        id = element;
+      }
+    });
+    //setting cookie
+    req.session.user_id = id;
+    //redirect to /urls
+    res.redirect('/urls');
+  }
+});
+
+//checks if email already exists and checks if field is empty
+app.post("/register", (req, res) => {
+  if (req.body.email === '' || req.body.password === '') {
+    //returns error message
+    res.status(400).send('<html><head><title>TinyAPP</title></head><body style="font-size:20px; padding:50px;"><p>Error 400</p><p>Please enter both an email and a password.</p><form action="/register" method="GET"><input id="Try again" type="submit" value="Try again"/></form></body></html>');
+  } else {
+      let arrEmail = [];
+      let arrID = Object.keys(users);
+      arrID.forEach(function(element) {
+        arrEmail.push((users[element].email));
+      });
+        if (arrEmail.includes(req.body.email)) {
+          //returns error message
+          res.status(400).send('<html><head><title>TinyAPP</title></head><body style="font-size:20px; padding:50px;"><p>Error 400</p><p>This email address is already in use.</p><form action="/register" method="GET"><input id="Try again" type="submit" value="Try again"/></form></body></html>');
+        } else {
+            //generates random alphanumeric string
+            let random = generateRandomString();
+            //adds new user to users object
+            users[random] = {};
+            users[random]['id'] = random;
+            users[random]['email'] = req.body.email;
+              //encrypts password with bcrypt
+            users[random]['password'] = bcrypt.hashSync(req.body.password, salt);
+            //setting cookie
+            req.session.user_id = random;
+            //redirects to /urls
+            res.redirect('/urls');
+        }
+  }
+});
+
+//POSTS to /urls
+app.post("/urls", (req, res) => {
+  //creates a new tinyURL
+  let random = generateRandomString();
+  urlDatabase[random] = {}
+  urlDatabase[random]['id'] = random;
+  urlDatabase[random]['url'] = req.body.longURL;
+  urlDatabase[random]['userID'] = req.session.user_id;
+  //redirects to edit page for new URL
+  res.redirect(`/urls/${random}`);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  let user_id = req.session.user_id;
+   if (urlDatabase[req.params.id] === undefined) {
+    res.sendStatus(403);
+  }
+  else if (user_id !== urlDatabase[req.params.id]['userID']) {
+    res.sendStatus(403);
+  }
+  else {
+    delete urlDatabase[req.params.id];
+    res.redirect('/urls');
+  }
+});
+
+app.post("/urls/:id", (req, res) => {
+  let user_id = req.session.user_id;
+
+  if (urlDatabase[req.params.id] === undefined) {
+    res.status(403).send('<html><head><title>TinyAPP</title></head><body style="font-size:20px; padding:50px;"><p>Error 403</p><form action="/urls" method="GET"><input id="Home" type="submit" value="Home"/></form></body></html>');
+  } else if (user_id !== urlDatabase[req.params.id]['userID']) {
+    res.status(403).send('<html><head><title>TinyAPP</title></head><body style="font-size:20px; padding:50px;"><p>Error 403</p><form action="/urls" method="GET"><input id="Home" type="submit" value="Home"/></form></body></html>');
+  } else {
+    urlDatabase[req.params.id]['url'] = req.body.longURL;
+    res.redirect('/urls');
+  }
+});
+
+app.post("/logout", (req, res) => {
+  delete req.session.user_id;
+  res.redirect(`/urls`);
+
+});
+
+//listens to specified port
+app.listen(PORT, () => {
+  console.log(`Connect to TinyApp through localhost:${PORT} !`);
+});
